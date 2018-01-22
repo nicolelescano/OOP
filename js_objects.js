2b2b77a120eaa88258c8fc236f80899f50e0f4e9
Nicole  Lescano <nl77656@srcs.loc>
2018-01-22 11:47:44 -0800

    first int

---
+++ js_objects.js
@@ -0,0 +1,17 @@
+function.cube("length, mass, volume, density, surfaceArea") {
+    this.length = 4;
+    this.mass = 2;
+    this.volume = fuction () {
+
+    }
+
+  }
+    this.density = function () {
+
+
+    }
+    this.surfacearea = surfaceArea
+
+
+
+var myCube = new Cube
