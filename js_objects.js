5beebfb53399547699446b4e5af909cebfd96645
Nicole  Lescano <nl77656@srcs.loc>
2018-01-22 11:48:48 -0800

    fix

--- js_objects.js
+++ js_objects.js
@@ -1,16 +1,16 @@
 function.cube("length, mass, volume, density, surfaceArea") {
     this.length = 4;
     this.mass = 2;
-    this.volume = fuction () {
+    this.volume = fuction() {
 
     }
 
-  }
-    this.density = function () {
+}
+this.density = function () {
 
 
-    }
-    this.surfacearea = surfaceArea
+}
+this.surfacearea = surfaceArea
 
 
 
